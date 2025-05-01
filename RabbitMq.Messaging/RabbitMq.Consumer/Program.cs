17c980c5c40a6258c877890447892c72b9a228ec
Chamikara <cmweerakoon@gmail.com>
2025-05-01 22:27:36 +0300

    publisher added

---
+++ RabbitMq.Messaging/RabbitMq.Consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "message",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
